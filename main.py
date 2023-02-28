a5e54aaf158dc65babdfe6184bf64414df572f96
Andriy Smetana <andriysmetana00@gmail.com>
2023-02-28 16:25:35 +0200

    add menu in the branch menu

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 print('Good morning!')
+
+
+
+while True:
+    print('Possible actions:')
+    print('1 - enter number\n2-change number\n3- zero out\n4-quit')
+    try:
+        choice = int(input('Enter your choice: '))
+    except ValueError:
+        print('Please, enter integer !!!')
