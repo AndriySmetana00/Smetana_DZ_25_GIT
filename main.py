8cd0d366a57297e1d71fda1ba253fce602b7ebf8
Andriy Smetana <andriysmetana00@gmail.com>
2023-02-28 16:45:27 +0200

    add if-elif-else to while cycle in menu

--- main.py
+++ main.py
@@ -7,5 +7,16 @@ while True:
     print('1 - enter number\n2-change number\n3- zero out\n4-quit')
     try:
         choice = int(input('Enter your choice: '))
+        if choice == 1:
+            num = int(input("Enter your number: "))
+        elif choice == 2:
+            new_num = int(input("Enter new number: "))
+            num = new_num
+        elif choice == 3:
+            num = 0
+        elif choice == 4:
+            break
+        else:
+            print('Wrong choice! Please, enter 1, 2, 3 or 4')
     except ValueError:
         print('Please, enter integer !!!')
